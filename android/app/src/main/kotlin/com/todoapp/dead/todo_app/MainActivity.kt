db3bfa8a9fe5fa7410bf0ef18094819dfa141637
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-10-12 14:21:35 +0200

    Initial Commit.

---
+++ android/app/src/main/kotlin/com/todoapp/dead/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.todoapp.dead.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/layout/HomeLayout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
